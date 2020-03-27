a4d483abd842546b67755d81b3e5a774daf0c6df
Michaela Brydon <brydon1@kenyon.edu>
2020-03-26 23:17:28 -0400

    :tada: Added printAccountSnapshot and sendAccountSnapshot to JavaScriptBridge class

--- Backend_Code/JavaScriptBridge.cpp
+++ Backend_Code/JavaScriptBridge.cpp
@@ -1,17 +1,30 @@
 //File Name: JavaScriptBridge.cpp
 //Author: M Brydon, S Kim, S Canfield
-
 //Email Address: brydon1@kenyon.edu, kim3@kenyon.edu, canfield1@kenyon.edu
 //Project: Mount Vernon Water
 //Description: Sends output and gets input to and from JavaScript through Ajax
-//Last Changed: February 17, 2020
+//Last Changed: March 26, 2020
 
 #include "JavaScriptBridge.h"
 
-/*
-string JavaScriptBridge::printArtwork(Artwork artwork){
-  string result, sep("*");
-  result +=  to_string(artwork.getArtId()) + sep + artwork.getArtistInfo().author + sep + artwork.getArtistInfo().birthDeath + sep + artwork.getTitle() + sep + artwork.getArtStyle().technique + sep + artwork.getArtSetting().location + sep + artwork.getUrl() + sep + artwork.getArtStyle().form + sep + artwork.getArtStyle().type + sep + artwork.getArtStyle().school + sep + artwork.getArtSetting().timeframe + sep + artwork.getArtSetting().date + sep + to_string(artwork.getNumLikes()) + sep;
+// Final string will have sep at end
+string JavaScriptBridge::printAccountSnapshot(AccountSnapshot accountSnapshot){
+	string result, sep("*");
+	result += accountSnapshot.getAccountNo() + sep + accountSnapshot.getResAddress().author + sep; 
+
+	// Has comments
+	if (accountSnapshot.getHasComments()){
+		result += "T" + sep;
+	} else {
+		result += "F" + sep;
+	}
+
+	// Has text
+	if (accountSnapshot.getHasText()){
+		result += "T" + sep;
+	} else {
+		result += "F" + sep;
+	}
 
    return result;
 }
@@ -23,8 +36,17 @@ string JavaScriptBridge::getElement(string element, Cgicc &cgi){
 
   return returnElement;
 }
-*/
 
-void JavaScriptBridge::sendString(string message){
+void JavaScriptBridge::sendAccountSnapshots(vector<AccountSnapshot> searchResults){
+	AccountSnapshot accountSnapshot;
+  	string jsMessage = "";
+	for (int i=0; i<searchResults.size(); i++){
+	    accountSnapshot = searchResults.at(i);
+	    jsMessage += printAccountSnapshot(accountSnapshot);
+	}
+	sendMessage(jsMessage);
+}
+
+void JavaScriptBridge::sendMessage(string message){
   cout << "Content-Type: text/plain\n\n" << message;
 }
--- Backend_Code/JavaScriptBridge.h
+++ Backend_Code/JavaScriptBridge.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include "AccountSnapshot.h"
 
 #include "cgicc/Cgicc.h"
 #include "cgicc/HTTPHTMLHeader.h"
@@ -12,19 +13,23 @@ using namespace cgicc;
 
 class JavaScriptBridge{
   public:
+    string printAccountSnapshot(AccountSnapshot accountSnapshot);
     //Preconditions: artwork must be an Artwork object
     //Postconditions: Returns artwork info delimited by * as a string
-    //string printArtwork(Artwork artwork);
-
+    
+    string getElement(string element, Cgicc &cgi);
     //Preconditions: element must be a string corresponding to an ajax element
     //sent from JavaScript. cgi is a Cgicc ajax object.
     //Postconditions: Returns the value of the ajax element as a string.
-    //string getElement(string element, Cgicc &cgi);
 
+    void sendAccountSnapshots(vector<AccountSnapshot> searchResults);
+    //Preconditions: 
+    //Postconditions:
+
+    void sendMessage(string message);
     //Preconditions: stringForJS must be a string that is intended to be sent
     //to a JavaScript file linked through Ajax.
     //Postconditions: String will be sent to a JavaScript file using a cout
-    void sendString(string message);
 };
 
 #endif //JAVASCRIPTBRIDGE_H
