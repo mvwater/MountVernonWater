d515bcb7c39270267fde3eb3d00dbeb423d67542
Michaela Brydon <brydon1@kenyon.edu>
2020-03-29 12:27:54 -0400

    :bug: Database will not connect

--- Backend_Code/DatabaseBridge.h
+++ Backend_Code/DatabaseBridge.h
@@ -16,8 +16,8 @@ using std::string;
 #ifndef DATABASEBRIDGE_H
 #define DATABASEBRIDGE_H
 
-//#define HOST "174.138.60.6:3306" // Set IP address instead of localhost
-#define HOST "localhost"
+#define HOST "174.138.60.6:3306" // Set IP address instead of localhost
+//#define HOST "localhost"
 #define USER "brydon1" // Maybe rename
 #define DB "mvwater" 
 #define PASS "clangrelationsheet" 
