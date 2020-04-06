fd00032bc18f16f0b2ff719ece795cf80d969e11
Michaela Brydon <brydon1@kenyon.edu>
2020-04-05 21:09:17 -0400

    :bug: Forgot to input query in DatabaseBridge member function in TestDatabase.cpp

--- Backend_Code/Testing/TestDatabase.cpp
+++ Backend_Code/Testing/TestDatabase.cpp
@@ -9,10 +9,11 @@ using std::cout;
 
 int main(){
 	string searchVal = "Delaware";
+	string query("SELECT * FROM accounts WHERE CONCAT(TAdd1, ' ', TAdd2, ' ', TAdd3) LIKE '%" + searchVal + "%';");
 	DatabaseBridge databaseBridge;
 	// Receive vector of AccountSnapshot objects from database
 	//vector<AccountSnapshot> searchResults = databaseBridge.searchByAddress(searchVal);
-	vector<AccountSnapshot> searchResults = databaseBridge.queryDatabase(searchVal);
+	vector<AccountSnapshot> searchResults = databaseBridge.queryDatabase(query);
 
 	JavaScriptBridge javaScriptBridge;
 	cout << javaScriptBridge.accountSnapshotsToStr(searchResults) << endl;
