e35f8fbef370d66ab0d3c7e87afcacb768f00253
Michaela Brydon <brydon1@kenyon.edu>
2020-04-14 14:42:28 -0400

    Edited js

--- Web_Code/assets/js/Search.js
+++ Web_Code/assets/js/Search.js
@@ -96,7 +96,7 @@ function showSearchResults(resultString){
     var listLength = accountData.length;
 
     if (listLength < 6){
-        return "<h3>Internal Error</h3>";
+        return "<h3>This address is not in the system.</h3>";
     } else {
         console.log("We have results.");
         console.log(listLength);
