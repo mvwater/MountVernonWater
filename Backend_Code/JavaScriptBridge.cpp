cc95d52beebfd3f56b335d205f651063c860dd78
Shane Canfield <scanfield413@gmail.com>
2020-04-19 19:27:34 -0400

    added more detail to accountInfo

--- Backend_Code/JavaScriptBridge.cpp
+++ Backend_Code/JavaScriptBridge.cpp
@@ -3,14 +3,14 @@
 //Email Address: brydon1@kenyon.edu, kim3@kenyon.edu, canfield1@kenyon.edu
 //Project: Mount Vernon Water
 //Description: Sends output and gets input to and from JavaScript through Ajax
-//Last Changed: March 26, 2020
+//Last Changed: 19 April 2020
 
 #include "JavaScriptBridge.h"
 
 // Final string will have sep at end
 string JavaScriptBridge::printAccountInfo(AccountInfo accountInfo){
 	string result, sep("*");
-	result += accountInfo.getAccountNo() + sep + accountInfo.getStatus()+ sep + accountInfo.getstartDate() + sep + accountInfo.getResident().name + sep + accountInfo.getLandlord().name; 
+	result += accountInfo.getAccountNo() + sep + accountInfo.getStatus()+ sep + accountInfo.getstartDate() + sep + accountInfo.getResident().name + sep + accountInfo.getResident().address.add1 + sep + accountInfo.getResident().address.add2 + sep + accountInfo.getResident().address.add3 + sep + accountInfo.getResident().address.city + sep + accountInfo.getResident().address.state + set + accountInfo.getResident().address.zip + sep + accountInfo.getResident().phoneNum + sep + accountInfo.getResident().email + sep + accountInfo.getResident().SScan + sep + accountInfo.getResident().DLNum + sep + accountInfo.getResident().cellnum + sep + accountInfo.getResident().dob + sep + accountInfo.getLandlord().name + sep + accountInfo.getLandlord().address.add1 + sep + accountInfo.getLandlord().address.add2 + sep + accountInfo.getLandlord().address.add3 + sep + accountInfo.getLandlord().address.city + sep + accountInfo.getLandlord().address.state + set + accountInfo.getLandlord().address.zip + sep + accountInfo.getLandlord().phoneNum + sep + accountInfo.getLandlord().email + sep + accountInfo.getLandlord().SScan + sep + accountInfo.getLandlord().DLNum + sep + accountInfo.getLandlord().cellnum + sep + accountInfo.getLandlord().dob; 
 
 	// Has comments
 	//if (accountInfo.getHasComments()){
