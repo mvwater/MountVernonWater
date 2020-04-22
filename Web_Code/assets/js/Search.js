16889573ecf5ee6100d61a8721090b08a62d0770
Michaela Brydon <brydon1@kenyon.edu>
2020-04-22 18:47:43 -0400

    :art: Trying out different account page looks

--- Web_Code/assets/js/Search.js
+++ Web_Code/assets/js/Search.js
@@ -229,7 +229,7 @@ function displayAccountInfo(resultString){
 
 
         for (var i = 0; i < 15; i++){
-            result += "<p id=res_'" + idNameList[i] + "'>"+labelNameList[i]+":&nbsp;<span style='color:white'>" + accountData[i+1] + "</span></p>"
+            result += "<p id=res_'" + idNameList[i] + "'><bold>"+labelNameList[i]+":&nbsp;</bold> " + accountData[i+1] + "</p>"
         }
 
         result += "</div></div></div></div>";
