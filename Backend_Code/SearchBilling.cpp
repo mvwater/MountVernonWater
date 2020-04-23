e9c3bbafdb131d0d4a53be0b862f206e6d5c56d6
Shane <scanfield413@gmail.com>
2020-04-23 18:42:04 -0400

    okay im done after this for a bit

---
+++ Backend_Code/SearchBilling.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "DatabaseBridge.h"
+#include "JavaScriptBridge.h"
+#include "AccountInfo.h"
+#include "BillingInfo.h"
+
+// Stuff for Ajax
+#include "cgicc/Cgicc.h"
+#include "cgicc/HTTPHTMLHeader.h"
+#include "cgicc/HTMLClasses.h"
+
+// Fix later
+using namespace cgicc;
+//using std::cout;
+
+int main(){
+  Cgicc cgi; // Ajax object
+  JavaScriptBridge javaScriptBridge;
+
+  //Receive info from web page
+
+  string searchVal = javaScriptBridge.getElement("accountNo", cgi);
+  DatabaseBridge databaseBridge;
+
+  // Receive a BillingInfo object from from database
+  BillingInfo searchResult = databaseBridge.billingInfoByAccountNo(searchVal);
+
+  // Send search results to JavaScript 
+  javaScriptBridge.sendBillingInfos(searchResult);
+  return 0;
+}
